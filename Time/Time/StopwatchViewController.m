ee78114259d28ce2ebd03da60b93f82b79676781
Henna <henna.ahmed92@gmail.com>
2015-08-22 16:48:28 -0400

    finished stopwatch

--- Time/Time/StopwatchViewController.m
+++ Time/Time/StopwatchViewController.m
@@ -14,6 +14,16 @@
 @property (weak, nonatomic) IBOutlet UIButton *lapButton;
 @property (weak, nonatomic) IBOutlet UITableView *stopWatchTableView;
 @property (nonatomic) NSMutableArray * data;
+@property (nonatomic) BOOL initialStart;
+@property (nonatomic) BOOL start;
+@property (nonatomic) BOOL isLapThere;
+
+
+@property int millisecondsElapsed;
+@property (weak, nonatomic) IBOutlet UILabel *stopwatchLabel;
+@property (strong, nonatomic) NSTimer *stopWatchTimer;
+
+
 
 @end
 
@@ -21,10 +31,14 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.initialStart = YES;
+    self.isLapThere = YES;
     self.data = [[NSMutableArray alloc]init];
     [self.data addObject:@"90"];
     [self.data addObject:@"40"];
     
+    [self.startButton setTitleColor: [UIColor greenColor] forState:UIControlStateNormal];
+    self.lapButton.alpha = 0.5;
     self.startButton.layer.cornerRadius = self.startButton.bounds.size.width/2;
     self.lapButton.layer.cornerRadius = self.lapButton.bounds.size.width/2;
     [[self.lapButton layer] setBorderWidth:2.0f];
@@ -32,7 +46,7 @@
     [[self.startButton layer] setBorderWidth:2.0f];
     [[self.startButton layer] setBorderColor: [UIColor blackColor].CGColor];
     
-
+    
     self.stopWatchTableView.delegate = self;
     self.stopWatchTableView.dataSource = self;
     
@@ -45,9 +59,91 @@
     // Dispose of any resources that can be recreated.
 }
 - (IBAction)captureLapTime:(UIButton *)sender {
-    NSNumber * number = [NSNumber numberWithUnsignedInt:arc4random_uniform(exp2(32)-1)];
-    [self.data addObject:[number stringValue]];
-    [self.stopWatchTableView reloadData];
+    if(self.isLapThere){
+        
+        [self.data addObject:self.stopwatchLabel.text];
+        [self.stopWatchTableView reloadData];
+    }
+    else{
+        [self.startButton setTitle:@"Start" forState:UIControlStateNormal];
+        [self.startButton setTitleColor: [UIColor greenColor] forState:UIControlStateNormal];
+        self.lapButton.enabled =NO;
+        [self.lapButton setTitle:@"Lap" forState:UIControlStateNormal];
+        self.initialStart = YES;
+        self.lapButton.alpha = 0.5;
+        
+        [self.data removeAllObjects];
+        [self.stopWatchTableView reloadData];
+        
+        
+        
+        [self.stopWatchTimer invalidate];
+        self.stopWatchTimer = nil;
+        self.millisecondsElapsed = 0;
+        self.stopwatchLabel.text = [NSString stringWithFormat:@"00:00:%02d", self.millisecondsElapsed];
+    }
+}
+
+- (void)updateTimer
+{
+    self.millisecondsElapsed += 1;
+    
+    int minutes = self.millisecondsElapsed / 6000;
+    int remainder_m = self.millisecondsElapsed % 6000; //remaining milliseconds
+    
+    
+    int seconds =   remainder_m/100 ; // seconds
+    int remainder_s = remainder_m % 100; //remaining seconds
+    
+    int millisec = remainder_s;
+    
+    
+    
+    self.stopwatchLabel.text = [NSString stringWithFormat:@"%02d:%02d:%02d", minutes, seconds, millisec];
+}
+
+- (IBAction)startButtonTapped:(UIButton *)sender {
+    if(self.initialStart){
+        self.initialStart = NO;
+        self.start = NO;
+        [self.startButton setTitle:@"Stop" forState:UIControlStateNormal];
+        [self.startButton setTitleColor: [UIColor redColor] forState:UIControlStateNormal];
+        self.lapButton.enabled = YES;
+        self.lapButton.alpha = 1.0;
+        self.isLapThere = YES;
+        
+        //NSTimer stuff
+        self.stopWatchTimer = [NSTimer scheduledTimerWithTimeInterval:0.01
+                                                               target:self
+                                                             selector:@selector(updateTimer)
+                                                             userInfo:nil
+                                                              repeats:YES];
+    }
+    else if(!self.start){
+        self.start = YES;
+        
+        [self.startButton setTitle:@"Start" forState:UIControlStateNormal];
+        [self.startButton setTitleColor: [UIColor greenColor] forState:UIControlStateNormal];
+        [self.lapButton setTitle:@"Reset" forState:UIControlStateNormal];
+        self.isLapThere = NO;
+        
+        [self.stopWatchTimer invalidate];
+        
+    }
+    else{
+        self.start = NO;
+        
+        [self.startButton setTitle:@"Stop" forState:UIControlStateNormal];
+        [self.startButton setTitleColor: [UIColor redColor] forState:UIControlStateNormal];
+        [self.lapButton setTitle:@"Lap" forState:UIControlStateNormal];
+        self.isLapThere = YES;
+        
+        self.stopWatchTimer = [NSTimer scheduledTimerWithTimeInterval:0.01
+                                                               target:self
+                                                             selector:@selector(updateTimer)
+                                                             userInfo:nil
+                                                              repeats:YES];
+    }
 }
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
@@ -71,13 +167,13 @@
 }
 
 /*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 @end
